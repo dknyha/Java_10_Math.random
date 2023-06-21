55c10ca8dfd4eafe971df8be4dc886c4ba9d0ec7
Dima Kniga <kniga.dima.2003@gmail.com>
2023-06-22 01:04:46 +0300

    edde

--- src/section_10.java
+++ src/section_10.java
@@ -352,7 +352,19 @@ public class section_10 {
 
     public static void exercise10_17() {
         space();
-
+        // здесь использовали округление именно Math.round(), которое округляет числа,
+        // до ближайшего целого
+        double min = 0.1, max = 10.5, S, P, gipotenuza;
+        double katet1 = (double) (Math.random() * ((max - min) + 1) + min);
+        double katet2 = (double) (Math.random() * ((max - min) + 1) + min);
+        System.out.println("Первый катет = " + katet1 + "\nВторой катет = " + katet2);
+        gipotenuza = Math.sqrt((katet1 * katet1) + (katet2 * katet2));
+        S = 0.5 * katet1 * katet2;
+        P = katet1 + katet2 + gipotenuza;
+        System.out.println(
+                "Гипотенуза = " + gipotenuza + "\nS = " + S + "\nP = " + P);
+        System.out.println("Округленные значения\nГипотенуза = " + Math.round(gipotenuza) + "\nS = " + Math.round(S)
+                + "\nP = " + Math.round(P));
     }
 
     public static void exercise10_18() {
